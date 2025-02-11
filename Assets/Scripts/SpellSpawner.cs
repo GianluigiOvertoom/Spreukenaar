9acc3167fa727184c0334c32fc1afc3287c2485a
SuperGamer-NL <archeolooglucas@gmail.com>
2025-02-11 21:17:21 +0100

    beginnetje random spell spawner

--- Assets/Scripts/SpellSpawner.cs
+++ Assets/Scripts/SpellSpawner.cs
@@ -1,7 +1,5 @@
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
+using System;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class SpellSpawner : MonoBehaviour {
     //reference spell prefab
@@ -15,8 +13,62 @@ public class SpellSpawner : MonoBehaviour {
     private bool randomPosReset = true;
 
     private void Start() {
-        minPos = new Vector3(-6,5,1);
-        maxPos = new Vector3(6,5,7);
+        GenerateRandomSpell();
+    }
+
+    private enum Element {
+        Vuur, 
+        Water, 
+        Aarde, 
+        Wind
+    }
+
+    private Element spreukenaarType;
+
+    private void GenerateRandomSpell() {
+        // pick random value from enum spreukenaarType
+        /*  1. Kijk naar de length van de enum v
+            2. Pak deze length en zet deze als maxSpreukenaarType v
+            3. Kies random getallen tussen 0 en maxSpreukenaarType v
+            4. Gebruik je randomized getal, dat getal als var toString en switchcase dat....*/
+        
+        
+        // enumLength = Enum.GetValues(typeof(SpreukenaarType));
+
+        //define start of enum count
+        int enumLength = 0;
+
+        foreach(Element spreukenaarType in Enum.GetValues(typeof(Element))) {
+            enumLength++;
+        }
+
+        int maxSpreukenaarType = enumLength;
+
+        //generate random int between 0 and enumlength
+        int randomEnumInt = UnityEngine.Random.Range(0, maxSpreukenaarType + 1);
+        
+        string randomEnumIntString = randomEnumInt.ToString();
+        
+
+        Debug.Log("SpreukenaarTypeInt = " + randomEnumInt + " bijbehorende enum string = " + Enum.Parse<Element>(randomEnumIntString));
+    }
+
+    //Deze functie moet eigenlijk in een spreukenaarStateManager script ofzo...
+    private void HandleState() {
+        switch (spreukenaarType) {
+            case Element.Vuur:
+                // switch spreukenaar logic
+                break;
+            case Element.Water:
+                // switch spreukenaar logic
+                break;
+            case Element.Aarde:
+                // switch spreukenaar logic
+                break;
+            case Element.Wind:
+                // switch spreukenaar logic
+                break;
+        }
     }
 
     private void Timer() {
@@ -26,11 +78,14 @@ public class SpellSpawner : MonoBehaviour {
     }
 
     private void RandomPositionGenerator() {
+        minPos = new Vector3(-6,5,1);
+        maxPos = new Vector3(6,5,7);
+
         if(randomPosReset == true) {
             randomPosition = new Vector3(
-            Random.Range(minPos.x, maxPos.x),
-            Random.Range(minPos.y, maxPos.y),
-            Random.Range(minPos.z, maxPos.z)
+            UnityEngine.Random.Range(minPos.x, maxPos.x),
+            UnityEngine.Random.Range(minPos.y, maxPos.y),
+            UnityEngine.Random.Range(minPos.z, maxPos.z)
             );
             
             randomPosReset = false;
@@ -39,20 +94,20 @@ public class SpellSpawner : MonoBehaviour {
 
     private void SpawnSpell() {
         randomPosReset = true;
-        GameObject spellInstantiate = Instantiate(spell, randomPosition, Quaternion.identity);
+        Instantiate(spell, randomPosition, Quaternion.identity);
         spellCount += 1;
     }
 
     private void SpellCheck() {
         if(spellCount == maxSpellAmount) {
-            spellSpawnIntervall = Random.Range(5,10);
+            spellSpawnIntervall = UnityEngine.Random.Range(5,10);
         }
     }
 
     private void TimeCheck() { 
         if(spellSpawnIntervall <= 0) {
             SpawnSpell();
-            spellSpawnIntervall = Random.Range(5,10);
+            spellSpawnIntervall = UnityEngine.Random.Range(5,10);
         }
     }
 
