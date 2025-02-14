fa10d43fe64aebc13223f23b6488a05447f9cc03
SuperGamer-NL <archeolooglucas@gmail.com>
2025-02-14 11:14:52 +0100

    Final sorting layer fixes

--- Assets/Scripts/ActiveLayerSorter.cs
+++ Assets/Scripts/ActiveLayerSorter.cs
@@ -1,4 +1,5 @@
 using System;
+using NUnit.Framework;
 using UnityEngine;
 
 public class ActiveLayerSorter : MonoBehaviour {
@@ -10,6 +11,11 @@ public class ActiveLayerSorter : MonoBehaviour {
 
     private void AdjustSortingLayer() {
         spriteRenderer.sortingOrder = -(int)(transform.position.z * 100);
+        if(spriteRenderer.sortingOrder < -1400) {
+            spriteRenderer.sortingLayerID = default;
+        } else {
+            spriteRenderer.sortingLayerID = 2048447561;
+        }
     }
 
     private void Update() {
